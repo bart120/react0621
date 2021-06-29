6694c730d109df2301055346d2c1d7fa53368c70
Vincent <vleclerc@inow.fr>
2021-06-29 11:57:25 +0200

    props, event

---
+++ src/core/components/forms/InputMail.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { Form } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+
+const regMail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+export default class InputMail extends Component {
+
+    static propTypes = {
+        name: PropTypes.string.isRequired,
+        placeholder: PropTypes.string,
+        validate: PropTypes.bool,
+        onChange: PropTypes.func
+    }
+
+    state = { isInvalid: false };
+
+    change = (ev) => {
+        if (this.props.validate) {
+            const test = regMail.test(ev.target.value);
+            this.setState({ isInvalid: !test });
+            this.props.onChange(ev, test);
+        } else {
+            this.props.onChange(ev);
+        }
+    }
+
+    render() {
+        console.log(this.props);
+
+        return (
+            <>
+                <Form.Control type="email" placeholder={this.props.placeholder}
+                    name={this.props.name} onChange={this.change} isInvalid={this.state.isInvalid} />
+                <Form.Control.Feedback type="invalid">
+                    {this.props.children}
+                </Form.Control.Feedback>
+            </>
+        )
+    }
+}
--- src/pages/authentication/Login.jsx
+++ src/pages/authentication/Login.jsx
@@ -1,10 +1,50 @@
 import React, { Component } from 'react'
+import { Button, Form } from 'react-bootstrap'
+import InputMail from '../../core/components/forms/InputMail';
 
 export default class Login extends Component {
+
+    state = { login: '', password: '' };
+
+    /*constructor() {
+        super();
+        this.changeFormField = this.changeFormField.bind(this);
+    }*/
+
+    changeFormField = (ev, isValid) => {
+        //console.log(ev.target.name);
+        //console.log(this);
+        this.setState({ [ev.target.name]: ev.target.value });
+        console.log(isValid);
+    }
+
+
+    submit = (ev) => {
+        ev.preventDefault();
+        console.log(this.state);
+    }
+
     render() {
         return (
             <div>
                 <h1>Authentification</h1>
+                <Form noValidate onSubmit={this.submit}>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Login</Form.Label>
+                        {/*<Form.Control type="email" placeholder="Votre email" name="login" onChange={this.changeFormField} />*/}
+                        <InputMail placeholder="Votre email" name="login" onChange={this.changeFormField} validate>
+                            <div>Email invalide</div>
+                        </InputMail>
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Mot de passe</Form.Label>
+                        <Form.Control type="password" placeholder="Votre mot de passe" name="password" onChange={this.changeFormField} />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Se connecter
+                    </Button>
+                </Form>
             </div>
         )
     }
